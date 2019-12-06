1f6b4f64f92ed03eae3f2d9070938c91e454c9fc
Tsung-Ta Wu <edison1998402@gmail.com>
2019-12-06 09:43:47 +0800

    ans.txt

--- dataset/convolution.py
+++ dataset/convolution.py
@@ -23,6 +23,23 @@ def sqrDiff(img, pattern):
     result += mul(one(img) * mask, sqr(pattern))
     return result
 
+def toNumber(targets):
+    result = 0
+    h, w = targets[0].shape
+    x, y = 0, 0
+    while y < w:
+        while y < w and x < h:
+            for i in range(10):
+                if x >= targets[i].shape[0] or y >= targets[i].shape[1]:
+                    continue
+                if targets[i][x][y] >= 0:
+                    result = 10 * result + i
+                    y += 8
+                    break
+            x = x+1
+        y, x = y+1, 0 
+    return result
+
 patterns = []
 for i in range(10):
     pattern = Image.open('../imgs/numbers/'+str(i)+'.png')
@@ -33,10 +50,10 @@ for j in range(64):
     img = Image.open('../imgs/00{:02}'.format(j+1) + '.png')
     img = np.array(img)[:,:,0].astype(int)
     result = [] 
-    print('HI', j+1)
     for i in range(10):
         d = sqrDiff(img, patterns[i])
-        d = np.where(d < 1000000, 1, 0)
+        d = np.where(d < 600000, i, -1)
         result.append(d)
+    print(j+1, toNumber(result))
 
 
