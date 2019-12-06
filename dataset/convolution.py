46178a91d30636b64e49cfa79ffa4ac77fc4bd9f
Tsung-Ta Wu <edison1998402@gmail.com>
2019-12-06 10:59:02 +0800

    short function to lambda

--- dataset/convolution.py
+++ dataset/convolution.py
@@ -5,17 +5,11 @@ import numpy as np
 import scipy.signal
 from PIL import Image
 
-def one(img):
-    return np.ones(img.shape, dtype=int)
-
-def sqr(img):
-    return img ** 2
-
-def mul(img, pattern):
-    return scipy.signal.fftconvolve(img, np.flip(pattern), mode='valid')
-#    return scipy.signal.convolve2d(img, np.flip(pattern), mode='valid')
-
 def sqrDiff(img, pattern):
+    one = lambda x: np.ones(x.shape, dtype=int)
+    sqr = lambda x: x ** 2
+    mul = lambda x, y: scipy.signal.fftconvolve(x, np.flip(y), mode='valid')
+    
     mask = np.where(img < 120, 0, 1)
     result = 0
     result += mul(sqr(img) * mask, one(pattern)) 
@@ -44,11 +38,11 @@ for i in range(10):
     patterns.append(pattern)
 
 for j in range(64):
-    img = Image.open('../imgs/00{:02}'.format(j+1) + '.png')
+    img = Image.open('../imgs/{:05}'.format(j+1) + '.png')
     img = np.array(img)[:,:,0].astype(int)
     result = [] 
     for i in range(10):
         d = sqrDiff(img, patterns[i])
         d = np.where(d < 600000, i, -1)
         result.append(d)
-    print(j+1, toNumber(result))
+    print('{:04}'.format(toNumber(result)))
