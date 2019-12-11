9b8d73aa8f45f2d2861d553a8a6d63e732f531a1
Tsung-Ta Wu <edison1998402@gmail.com>
2019-12-11 15:07:05 +0800

    rewrite download.sh

--- dataset/download.sh
+++ dataset/download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-for i in {00065..09999}; do
+for i in {00000..00099}; do
    curl https://e3new.nctu.edu.tw/theme/dcpc/securimage/securimage_show.php?0.11112242575906661 --output $i.png
    sleep 0.1
 done
