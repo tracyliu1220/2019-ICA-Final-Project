54c5e5094364eb593f4582d13be6f6642a10f0a8
Tsung-Ta Wu <edison1998402@gmail.com>
2019-12-11 15:01:07 +0800

    refactor convolution.py

--- dataset/convolution.py
+++ dataset/convolution.py
@@ -7,12 +7,12 @@ from PIL import Image
 
 def sqrDiff(img, pattern):
     one = lambda x: np.ones(x.shape, dtype=int)
-    sqr = lambda x: x ** 2
+    sqr = lambda x: x**2
     mul = lambda x, y: scipy.signal.fftconvolve(x, np.flip(y), mode='valid')
-    
+
     mask = np.where(img < 120, 0, 1)
     result = 0
-    result += mul(sqr(img) * mask, one(pattern)) 
+    result += mul(sqr(img) * mask, one(pattern))
     result -= 2 * mul(img * mask, pattern)
     result += mul(one(img) * mask, sqr(pattern))
     return result
@@ -28,26 +28,38 @@ def toNumber(targets):
         if arr[i] >= 0:
             result = 10 * result + arr[i]
             i += 8
-        i += 1 
+        i += 1
     return result
 
-def main():
-    patterns = []
+patterns = []
+
+def readPatterns():
+    if len(patterns) > 0:
+        return
+
     for i in range(10):
-        pattern = Image.open('../imgs/numbers/'+str(i)+'.png')
+        pattern = Image.open('../imgs/numbers/' + str(i) + '.png')
         pattern = np.array(pattern).astype(int)
         patterns.append(pattern)
 
-    for j in range(10000):
-        img = Image.open('../imgs/{:05}'.format(j) + '.png')
-        img = np.array(img)[:,:,0].astype(int)
-        result = [] 
-        for i in range(10):
-            d = sqrDiff(img, patterns[i])
-            d = np.where(d < 600000, i, -1)
-            result.append(d)
-        print('{:04}'.format(toNumber(result)))
+def imgPathToNumber(image_path):
+    readPatterns()
+
+    img = Image.open(image_path)
+    img = np.array(img)[:, :, 0].astype(int)
+
+    result = []
+    for i in range(10):
+        d = sqrDiff(img, patterns[i])
+        d = np.where(d < 600000, i, -1)
+        result.append(d)
+    return toNumber(result)
+
+def main():
+    for j in range(100):
+        image_path = '../imgs/test/{:05}'.format(j) + '.png'
+        number = imgPathToNumber(image_path)
+        print('{:3} {:04}'.format(j, number))
 
 if __name__ == '__main__':
     main()
-
