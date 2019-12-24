4614a580a2b7b936503a5fb0dc2d713197f20338
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2019-12-24 23:16:18 +0800

    user download modified

--- dataset/download.sh
+++ dataset/download.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/bash
 
-for i in {000000..$1}; do
-   curl https://e3new.nctu.edu.tw/theme/dcpc/securimage/securimage_show.php?0.11112242575906661 --output $i.png
-   sleep 0.1
-done
+if [ $1 = "test" ]; then
+    for i in {000000..010000}; do
+       curl https://e3new.nctu.edu.tw/theme/dcpc/securimage/securimage_show.php?0.11112242575906661 --output $i.png
+       sleep 0.1
+    done
+fi
+
+if [ $1 = "train" ]; then
+    for i in {000000..200000}; do
+       curl https://e3new.nctu.edu.tw/theme/dcpc/securimage/securimage_show.php?0.11112242575906661 --output $i.png
+       sleep 0.1
+    done
+fi
