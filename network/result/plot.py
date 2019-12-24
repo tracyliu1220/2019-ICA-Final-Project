3d46f2bc3893146196fa79436d8e01f7b743968f
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2019-12-24 22:26:41 +0800

    plot modified

--- network/result/plot.py
+++ network/result/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
         
-f = open('test_result.txt', 'r')
+f = open('result.txt', 'r')
 lines = f.readlines()
 data = []
 
@@ -31,11 +31,31 @@ ax0_twin = ax[0].twinx()
 
 ax0_twin.set_ylabel('accuracy')
 ax0_twin.plot(x, train_acc, x, test_acc)
-ax0_twin.legend(('train acc', 'test acc'), loc='upper right')
+ax0_twin.legend(('train acc', 'test acc'), loc='lower right')
 
 ax[1].set_xlabel('epoch')
 ax[1].set_ylabel('# of training data')
 ax[1].plot(x, testdata_n)
 
 fig.tight_layout()
+plt.show()
+
+test_acc   = np.array([ float(data[i][3]) for i in range(0, 63) ])
+comb_acc   = np.array([ float(data[i][4]) for i in range(0, 63) ])
+
+digi_acc   = np.array([ float(data[i][5]) for i in range(0, 63) ])
+digi_ran   = np.array([ float(data[i][6]) for i in range(0, 63) ])
+
+fig, ax = plt.subplots(nrows=2, ncols=1)
+
+ax[0].set_xlabel('epoch')
+ax[0].set_ylabel('accuracy')
+ax[0].plot(x, test_acc, x, comb_acc)
+ax[0].legend(('permutation acc', 'combination acc'), loc='lower right')
+
+ax[1].set_xlabel('epoch')
+ax[1].set_ylabel('# of digits')
+ax[1].plot(x, digi_acc, x, digi_ran)
+ax[1].legend(('correct digits', 'digits in set'), loc='lower right')
+
 plt.show()
